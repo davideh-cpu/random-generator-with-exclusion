450d19fa9f13492f08df8538aa8f3f53e93ab7c7
davideh-cpu <78468536+davideh-cpu@users.noreply.github.com>
2022-01-29 14:31:57 +0000

    Update rdm.js

--- rdm.js
+++ rdm.js
@@ -4,6 +4,9 @@ generateBtn.addEventListener("click", generateRandomNum);
 function generateRandomNum() {
   const fromNum = document.querySelector(".fromNum").value;
   const toNum = document.querySelector(".toNum").value;
+  const a = //inset your number to exclude
+  const b = //nuber to get when the output is a
+  if (randomNum= a) randomNum = b
 
   randomNum = Math.floor(Math.random() * parseInt(toNum));
   if (randomNum >= parseInt(fromNum)) {
